8760be96cc55281071d4134294852d408979661f
Rishabhraj2754 <rishabhraj2754@gmail.com>
2023-03-17 23:20:59 +0530

    Added functionality to calculate wage for part time employee

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,20 @@ public class Main {
         int wage_per_hour = 20;
         int full_day_hour = 8;
         int total_wage;
+        int part_time_hours = 4;
 
-        double num = Math.floor(Math.random() * 10) % 2;
+        double num = Math.floor(Math.random() * 10) % 3;
 
         if (num == 1) {
             System.out.println("Employee is Present");
             total_wage = wage_per_hour * full_day_hour;
-            System.out.println( "Total wage per hour of employee is "+total_wage);
-        }
-        else{
+            System.out.println("Total wage for a day of employee is rs " + total_wage);
+        } else if (num ==2) {
+            System.out.println("Employee is working for part time");
+            total_wage = part_time_hours * wage_per_hour;
+            System.out.println("Total wage for a day of employee is rs " + total_wage);
+
+        } else {
             System.out.println("Employee is Absent");
         }
 
