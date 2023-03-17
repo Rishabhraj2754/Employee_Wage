467243cd7973ac1a60bac6978dc6afd3f712480f
Rishabhraj2754 <rishabhraj2754@gmail.com>
2023-03-17 22:56:56 +0530

    Added check function for present or absent

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
+        double num = Math.floor(Math.random() * 10) % 2;
+
+        if (num == 1) {
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
