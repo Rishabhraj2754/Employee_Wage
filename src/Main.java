5afaa3867b669cfdd10b5e78668b5044ef1e7042
Rishabhraj2754 <rishabhraj2754@gmail.com>
2023-03-23 19:44:49 +0530

    Added functinallity to terminate the program as condition is reached

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,46 @@ public class Main {
         int full_day_hour = 8;
         int total_wage;
         int part_time_hours = 4;
-        int total_working_day = 20;
+        int total_working_day = 0;
+        int total_working_hours = 0;
+        int max_working_hours = 100;
+        int max_working_day = 20;
 
-        int num = (int) Math.floor(Math.random() * 10) % 3;
+        while (true) {
+            int num = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (num) {
-            case 0: {
-                System.out.println("Employee is absent");
+
+            switch (num) {
+                case 0: {
+                    System.out.println("Employee is absent");
+                }
+                break;
+                case 1: {
+                    System.out.println("Employee is present for part time");
+                    total_wage = part_time_hours * wage_per_hour;
+                    System.out.println("Wage of employee for part time is " + total_wage);
+                    total_wage = total_working_day * part_time_hours * wage_per_hour;
+                    System.out.println("Wage for a moth for part time is " + total_wage);
+                    total_working_hours += 4;
+                }
+                break;
+
+                case 2: {
+                    System.out.println("Employee is present");
+                    total_wage = full_day_hour * wage_per_hour;
+                    System.out.println("Wage of employee for full time is " + total_wage);
+                    total_wage = total_working_day * full_day_hour * wage_per_hour;
+                    System.out.println("Wage for a moth for full time is " + total_wage);
+                    total_working_hours += 8;
+
+                }
+                break;
+
             }
-            break;
-            case 1: {
-                System.out.println("Employee is present for part time");
-                total_wage = part_time_hours * wage_per_hour;
-                System.out.println("Wage of employee for part time is " + total_wage);
-                total_wage = total_working_day * part_time_hours * wage_per_hour;
-                System.out.println("Wage for a moth for part time is "+total_wage);
-            }
-            break;
-
-            case 2: {
-                System.out.println("Employee is present");
-                total_wage = full_day_hour * wage_per_hour;
-                System.out.println("Wage of employee for full time is " + total_wage);
-                total_wage = total_working_day * full_day_hour * wage_per_hour;
-                System.out.println("Wage for a moth for full time is "+total_wage);
-
-            }
-            break;
-
+            total_working_day += 1;
+            if (total_working_hours >= max_working_hours || total_working_day >= max_working_day)
+                break;
         }
-
 
     }
 }
